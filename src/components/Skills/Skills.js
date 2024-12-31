6ea3ad410c27bb344377556b84c26adde375a1c6
rahul-11105 <rahullokhande11105@gmail.com>
2024-12-31 23:40:15 +0530

    About section created

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "../Skills/Skills.css";
-import reactimg from "../source/react_img.jpg"
+import reactimg from "../source/react.webp";
+import Backend_img from "../source/node.webp";
+import MERN_img from "../source/MERN-1.webp";
 const Skills = () => {
   return (
     <section id="skills">
@@ -9,7 +11,7 @@ const Skills = () => {
             I am skilled and passinated web developer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in <br/><b className="skilllist">HTML, CSS, JavaScript, React.js, Node.js, Express.js, MongoDB, SQL as well as Java.</b>
         </span>
         <div className="skillBars">
-            <div className="skillBar"> 
+            <div id="react" className="skillBar"> 
                 <img className ="skillBarImg" src={reactimg} alt="img"/>
                 <div className="skillBarTxt">
                     <h2>React Frontend Development.</h2>
@@ -17,16 +19,16 @@ const Skills = () => {
                 </div>
             </div>
 
-            <div className="skillBar"> 
-                <img className ="skillBarImg" src={reactimg} alt="img"/>
+            <div id="node" className="skillBar"> 
+                <img className ="skillBarImg" src={Backend_img} alt="img"/>
                 <div className="skillBarTxt">
                     <h2>Backend Development.</h2>
                     <p>Can handle the backend of websites using Node.js, Express.js, MongoDB and SQL.</p>
                 </div>
             </div>
 
-            <div className="skillBar"> 
-                <img className ="skillBarImg" src={reactimg} alt="img"/>
+            <div id="mern" className="skillBar"> 
+                <img className ="skillBarImg" src={MERN_img} alt="img"/>
                 <div className="skillBarTxt">
                     <h2>Full Stack Development</h2>
                     <p>Can build full stack web applications using MERN.</p>
