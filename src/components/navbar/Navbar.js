3668c8b4dacf658ff45e5a3bb0ae37b1b001871f
rahul-11105 <rahullokhande11105@gmail.com>
2024-12-31 15:16:50 +0530

    react app created

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="navbar">
+            <img className="logo" src="" alt="logo"/>
+            <div className="desktopMenu">
+
+            </div>
+            <button className="desktopMenuBtn">
+                <img className="desktopMenuImg" src="" alt="#"/>
+            </button>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
